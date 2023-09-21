c6b024e81c4c2e5ac2ac119e9738e2ca82192c36
Ganesh Rai <53878675+petermultiverse1998@users.noreply.github.com>
2023-09-21 23:46:54 +0545

    fft update

--- signal/fft_test.c
+++ signal/fft_test.c
@@ -1,40 +1,40 @@
 //
 // Created by peter on 9/20/2023.
 //
+#include <math.h>
 #include "fft.h"
 #include "stdio.h"
+#include "windows.h"
 
-void print(int N,const float array[]){
-    printf("=>");
-    for (int i = 0; i < N; ++i)
-        printf("%.2f ",array[i]);
-    printf("\n");
+static void copyToClipboard(const char *output) {
+//    const char* output = "Test\0abc";
+    const size_t len = strlen(output) + 1;
+    HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, len);
+    memcpy(GlobalLock(hMem), output, len);
+    GlobalUnlock(hMem);
+    OpenClipboard(0);
+    EmptyClipboard();
+    SetClipboardData(CF_TEXT, hMem);
+    CloseClipboard();
 }
 
 int main(){
-    int N = 4;
-    float x[]={1,2,3,4,5,6,7,8};
+    int N = 256;//Samples
+    float x[N];
+    float dt = 1.0f/(float)N;//sampling time
+    for (int i = 0; i < N; ++i)
+        x[i] = sinf(2.0f*(float)M_PI*dt*(float)i);//Sine Wave
+
     float X[N];
     float ph[N];
-    float y[N];
-
-    print(N,x);
-
-//    StaticFFT.dft(N,x,X,ph);
-//    printf("DFT\n");
-//    print(N,X);
-//    print(N,ph);
-//    printf("IDFT\n");
-//    StaticFFT.idft(N,X,ph,y);
-//    print(N,y);
 
     StaticFFT.fft(N,x,X,ph);
-    printf("FFT\n");
-    print(N,X);
-    print(N,ph);
-    printf("IFFT\n");
-    StaticFFT.ifft(N,X,ph,y);
-    print(N,y);
 
-
+    char str[10024];
+    int ptr = 0;
+    for (int i = 0; i < sizeof(X) / 4; ++i)
+        ptr += sprintf(str + ptr, "%f \n", x[i]);
+//        ptr += sprintf(str + ptr, "%0.4f \n",X[i]);
+//        ptr += sprintf(str + ptr, "%0.4f \n",ph[i]);
+    copyToClipboard(str);
 }
